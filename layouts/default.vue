9e8ded8cbc4bd128c2b1f0f16c691fca1f9d9ed5
avosalmon <avosalmon@gmail.com>
2021-01-21 21:22:23 +0800

    wip: add header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,285 @@
 <template>
   <div>
+    <header class="relative bg-white">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6">
+        <div
+          class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-center"
+        >
+          <h1 class="">
+            <a href="/" class="text-xl">はまさき書道教室 Singapore</a>
+          </h1>
+          <div class="-mr-2 -my-2 md:hidden">
+            <button
+              type="button"
+              class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              @click="showMenu = true"
+            >
+              <span class="sr-only">Open menu</span>
+              <!-- Heroicon name: menu -->
+              <svg
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <nav
+          class="hidden md:flex justify-center items-center bg-gray-100 py-4 space-x-6"
+        >
+          <a
+            href="#"
+            class="text-base font-medium text-gray-700 hover:text-gray-900"
+          >
+            ごあいさつ
+          </a>
+          <a
+            href="#"
+            class="text-base font-medium text-gray-700 hover:text-gray-900"
+          >
+            ニュース
+          </a>
+          <a
+            href="#"
+            class="text-base font-medium text-gray-700 hover:text-gray-900"
+          >
+            講師紹介
+          </a>
+          <a
+            href="#"
+            class="text-base font-medium text-gray-700 hover:text-gray-900"
+          >
+            コース
+          </a>
+          <a
+            href="#"
+            class="text-base font-medium text-gray-700 hover:text-gray-900"
+          >
+            ギャラリー
+          </a>
+          <a
+            href="#"
+            class="text-base font-medium text-gray-700 hover:text-gray-900"
+          >
+            お問い合わせ
+          </a>
+        </nav>
+      </div>
+
+      <!--
+        Mobile menu, show/hide based on mobile menu state.
+
+        Entering: "duration-200 ease-out"
+          From: "opacity-0 scale-95"
+          To: "opacity-100 scale-100"
+        Leaving: "duration-100 ease-in"
+          From: "opacity-100 scale-100"
+          To: "opacity-0 scale-95"
+      -->
+      <transition
+        enter-active-class="duration-200 ease-out"
+        enter-from-class="opacity-0 scale-95"
+        enter-to-class="opacity-100 scale-100"
+        leave-active-class="duration-100 ease-in"
+        leave-from-class="opacity-100 scale-100"
+        leave-to-class="opacity-0 scale-95"
+      >
+        <div
+          v-show="showMenu"
+          class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+        >
+          <div
+            class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50"
+          >
+            <div class="pt-5 pb-6 px-5">
+              <div class="flex items-center justify-between">
+                <div>
+                  <img
+                    class="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                    alt="Workflow"
+                  />
+                </div>
+                <div class="-mr-2">
+                  <button
+                    type="button"
+                    class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                    @click="showMenu = false"
+                  >
+                    <span class="sr-only">Close menu</span>
+                    <!-- Heroicon name: x -->
+                    <svg
+                      class="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div class="mt-6">
+                <nav class="grid gap-y-8">
+                  <a
+                    href="#"
+                    class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <!-- Heroicon name: chart-bar -->
+                    <svg
+                      class="flex-shrink-0 h-6 w-6 text-indigo-600"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                      />
+                    </svg>
+                    <span class="ml-3 text-base font-medium text-gray-900">
+                      Analytics
+                    </span>
+                  </a>
+
+                  <a
+                    href="#"
+                    class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <!-- Heroicon name: cursor-click -->
+                    <svg
+                      class="flex-shrink-0 h-6 w-6 text-indigo-600"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                      />
+                    </svg>
+                    <span class="ml-3 text-base font-medium text-gray-900">
+                      Engagement
+                    </span>
+                  </a>
+
+                  <a
+                    href="#"
+                    class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <!-- Heroicon name: shield-check -->
+                    <svg
+                      class="flex-shrink-0 h-6 w-6 text-indigo-600"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                      />
+                    </svg>
+                    <span class="ml-3 text-base font-medium text-gray-900">
+                      Security
+                    </span>
+                  </a>
+
+                  <a
+                    href="#"
+                    class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <!-- Heroicon name: view-grid -->
+                    <svg
+                      class="flex-shrink-0 h-6 w-6 text-indigo-600"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+                      />
+                    </svg>
+                    <span class="ml-3 text-base font-medium text-gray-900">
+                      Integrations
+                    </span>
+                  </a>
+
+                  <a
+                    href="#"
+                    class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                  >
+                    <!-- Heroicon name: refresh -->
+                    <svg
+                      class="flex-shrink-0 h-6 w-6 text-indigo-600"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                      />
+                    </svg>
+                    <span class="ml-3 text-base font-medium text-gray-900">
+                      Automations
+                    </span>
+                  </a>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </header>
+
     <Nuxt />
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+    }
+  },
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
