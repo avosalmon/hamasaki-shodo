19b9e029ce494c30455d07675ecb3691857a3b5a
avosalmon <avosalmon@gmail.com>
2021-01-23 15:25:17 +0800

    add message section

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,57 @@
 <template>
   <div>
     <section class="text-center">
-      <p class="mb-3 text-6xl">はまさき書道教室</p>
-      <p class="mb-4 text-6xl">Singapore</p>
-      <p class="mb-4 text-2xl">Supported by Learning Stage</p>
-      <p class="text-xl">3人までの少人数指導。River Valleyの小さな教室です。</p>
+      <p class="my-3 text-4xl md:text-6xl">はまさき書道教室</p>
+      <p class="mb-4 text-4xl md:text-6xl">Singapore</p>
+      <p class="mb-8 text-2xl">Supported by Learning Stage</p>
+      <p class="mb-12 text-xl">
+        <span>3人までの少人数指導。</span>
+        <br class="md:hidden" />
+        <span>River Valleyの小さな教室です。</span>
+      </p>
+      <p>
+        <img
+          src="https://static.wixstatic.com/media/f9201a_613c3f69aa2c4dde9a80de52e31cc225~mv2.jpg/v1/fill/w_1880,h_1000,al_c,q_85,usm_0.66_1.00_0.01/IMG_0917_JPG.webp"
+          alt="はまさき書道教室"
+        />
+      </p>
     </section>
+
+    <section id="message" class="flex flex-col md:flex-row mt-12">
+      <div class="mb-12 md:mb-0 md:p-10 md:bg-gray-100">
+        <p class="leading-7 mb-4 md:mb-12">
+          文字を上手に書きたい、美しく書きたい、という思いは、字を書きはじめたこどもから大人まで、多くの人が心の内に持っている願いではないでしょうか。<br />
+          文字の美しさは、文字の形の良さだけを指すものではありません。その線をかたちづくる”一筆一筆”に美しさが宿っていきます。<br />
+          「はまさき書道教室
+          Singapore」では、3人までの少人数制のきめ細やかな指導によって、文字の外形的な美しさはもちろん、それに止まらない個性の光る文字を目指していきます。
+        </p>
+        <p>
+          <a
+            href="https://www.learning-stage.com/japanese-calligraphy/"
+            target="_blank"
+            rel="noopener"
+            class="inline-block border border-black px-7 py-3 transition-colors duration-300 hover:bg-black hover:text-white"
+            >Learning Stage HP</a
+          >
+        </p>
+      </div>
+      <div>
+        <img
+          src="https://static.wixstatic.com/media/f9201a_662faba766b04b9884971e28e9505970~mv2.jpg/v1/fill/w_1120,h_1240,al_c,q_85,usm_0.66_1.00_0.01/IMG_0929.webp"
+          alt="筆"
+        />
+      </div>
+    </section>
+
+    <section id="teacher"></section>
+
+    <section id="news"></section>
+
+    <section id="course"></section>
+
+    <section id="gallery"></section>
+
+    <section id="contact"></section>
   </div>
 </template>
 
