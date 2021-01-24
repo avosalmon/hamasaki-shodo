8e8146c0d98b4fb7888b8016146c08fd6cec8a92
avosalmon <avosalmon@gmail.com>
2021-01-24 12:28:04 +0800

    add IG link

--- layouts/default.vue
+++ layouts/default.vue
@@ -195,6 +195,42 @@
                       お問い合わせ
                     </span>
                   </a>
+
+                  <a
+                    href="https://www.instagram.com/sg_shodo_mh/"
+                    target="_blank"
+                    rel="noopener"
+                    class="-m-3 p-3 flex justify-center rounded-md hover:bg-gray-50"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-instagram"
+                    >
+                      <rect
+                        x="2"
+                        y="2"
+                        width="20"
+                        height="20"
+                        rx="5"
+                        ry="5"
+                      ></rect>
+                      <path
+                        d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"
+                      ></path>
+                      <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                    </svg>
+                    <span class="ml-3 text-base font-medium text-gray-900">
+                      Instagram
+                    </span>
+                  </a>
                 </nav>
               </div>
             </div>
