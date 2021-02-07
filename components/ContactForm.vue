03afc076ec8e32e68ea94ecd6371a43c550550dc
avosalmon <avosalmon@gmail.com>
2021-02-07 15:31:26 +0800

    handle form submission

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -119,6 +119,54 @@
       >
         送信
       </button>
+      <div v-if="isSuccess" class="bg-green-50 p-4 mt-4">
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <!-- Heroicon name: solid/check-circle -->
+            <svg
+              class="h-5 w-5 text-green-400"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <div class="ml-3">
+            <p class="text-sm font-medium text-green-800">
+              送信が完了しました。
+            </p>
+          </div>
+          <div class="ml-auto pl-3">
+            <div class="-mx-1.5 -my-1.5">
+              <button
+                class="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
+              >
+                <span class="sr-only">Dismiss</span>
+                <!-- Heroicon name: solid/x -->
+                <svg
+                  class="h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </form>
 </template>
@@ -139,6 +187,8 @@ export default {
         school_year: '',
         message: '',
       },
+      isSuccess: false,
+      isError: false,
     }
   },
   validations: {
@@ -174,9 +224,12 @@ export default {
         await axios.post('/', this.encode(data), {
           headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         })
-        console.log('form has been submitted successfuly!')
+        this.resetForm()
+        this.isSuccess = true
+        this.isError = false
       } catch (error) {
-        console.log('error', error)
+        this.isSuccess = false
+        this.isError = true
       }
     },
     encode(data) {
@@ -185,6 +238,12 @@ export default {
           (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
         )
         .join('&')
+    },
+    resetForm() {
+      Object.keys(this.form).forEach((key) => {
+        this.form[key] = ''
+      })
+      this.$v.$reset()
     },
   },
 }
