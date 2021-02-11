98cf3633f3b4ec6f26c6efaabbef825077ff6e1d
avosalmon <avosalmon@gmail.com>
2021-02-11 22:06:36 +0800

    wip: add trial page

---
+++ pages/courses/trial.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex flex-col px-4 md:flex-row md:container md:mx-auto md:px-0">
+    <div class="flex-1 -mx-4 md:mx-0">
+      <div class="md:hidden">
+        <CldImage
+          public-id="hamasaki-shodo.com/suzuri"
+          aspectRatio="4:3"
+          crop="fill"
+          effect="sharpen:100"
+          quality="auto"
+          fetchFormat="auto"
+          alt="硯"
+          responsive
+        />
+      </div>
+      <div class="hidden md:block">
+        <CldImage
+          public-id="hamasaki-shodo.com/suzuri"
+          aspectRatio="1:1"
+          crop="fill"
+          effect="sharpen:100"
+          quality="auto"
+          fetchFormat="auto"
+          alt="硯"
+          responsive
+        />
+      </div>
+    </div>
+    <div class="flex-1 pt-10 md:pt-32">
+      <h2 class="text-2xl mb-1 text-center">体験講座</h2>
+      <p class="text-xl mb-8 text-center">（約60分）</p>
+      <p class="leading-7 md:text-center">
+        体験料＄40<br />
+        詳細はお問い合わせください。
+      </p>
+      <div class="flex justify-center mt-10">
+        <ButtonLink href="/#contact">お問い合わせ</ButtonLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonLink from '@/components/ButtonLink'
+
+export default {
+  components: {
+    ButtonLink,
+  },
+}
+</script>
