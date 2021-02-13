f54587a7093f19966ed8fe65c37cc3fddac8d623
avosalmon <avosalmon@gmail.com>
2021-02-13 11:57:07 +0800

    add v-click-outside directive

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   css: ['@/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/click-outside.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
