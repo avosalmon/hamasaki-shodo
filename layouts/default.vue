89cbcf0773526bfc6dbb485a37eb71a08cb01f69
avosalmon <avosalmon@gmail.com>
2021-02-13 14:22:38 +0800

    add IG link to the header

--- layouts/default.vue
+++ layouts/default.vue
@@ -79,9 +79,36 @@
       <Nuxt />
     </main>
     <footer class="mx-auto w-full md:w-2/3 lg:w-3/5 px-4 py-10 text-center">
-      <span>&copy; {{ currentYear }} はまさき書道教室 Singapore. </span>
-      <br class="md:hidden" />
-      <span>Supported by Learning Stage</span>
+      <p class="flex justify-center mb-8">
+        <a
+          href="https://www.instagram.com/sg_shodo_mh/"
+          target="_blank"
+          rel="noopener"
+          class="text-base font-medium text-gray-700 hover:text-gray-900"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-instagram"
+          >
+            <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+          </svg>
+        </a>
+      </p>
+      <p>
+        <span>&copy; {{ currentYear }} はまさき書道教室 Singapore. </span>
+        <br class="md:hidden" />
+        <span>Supported by Learning Stage</span>
+      </p>
     </footer>
   </div>
 </template>
