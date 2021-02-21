1f8d6695608c62a73c25dfb94f4b6bc6286d3153
avosalmon <avosalmon@gmail.com>
2021-02-21 14:28:12 +0800

    replace anchor tag with NuxtLink

--- pages/courses/workshop.vue
+++ pages/courses/workshop.vue
@@ -36,7 +36,7 @@
       <p class="text-xl mb-8 text-center">※不定期</p>
       <p class="leading-7 md:text-center">
         不定期・単発開催のワークショップです。<br />
-        開催情報は<a href="/#news" class="underline">「お知らせ」</a
+        開催情報は<NuxtLink to="/#news" class="underline">「ニュース」</NuxtLink
         >欄にて公開いたします。<br />
         団体様で開催ご希望の方はお問い合わせください。
       </p>
