65776914741efaf14d77eff46d148cc52665fc26
avosalmon <avosalmon@gmail.com>
2021-03-27 14:32:15 +0800

    fetch news from contentful

---
+++ components/News.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="leading-8">
+    <p class="mb-8 text-2xl">
+      {{ title }}
+    </p>
+    <time class="block mb-8 text-lg" :datetime="publishDate">
+      {{ publishDate | date }}
+    </time>
+    <div v-html="body"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    publishDate: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -122,54 +122,14 @@
     <div id="news" class="mb-24"></div>
     <PageSection class="py-8">
       <h2 class="mb-20 text-4xl text-center">ニュース</h2>
-      <ul>
-        <!-- TODO: Extract as "News" component -->
-        <li class="leading-8 mb-24">
-          <p class="mb-8 text-2xl">
-            （締め切りました）【学生クラス】3日間集中書道レッスン 生徒さん募集
-          </p>
-          <time class="block mb-8 text-lg" datetime="2021-03-09">
-            2021年3月9日
-          </time>
-          <p class="mb-4">
-            3日間連続集中の小学生対象お習字講座を開催いたします。<br />
-            3名限定のため、ご興味のある方はお早めにお問い合わせ下さい。
-          </p>
-          <p class="mb-4">
-            日時：3/30(火)〜4/1(木) 14:00〜15:00<br />
-            場所：River Valleyエリアの講師自宅（Valley Point付近）<br />
-            対象：小学生以上で3日間全日程参加出来る方<br />
-            費用：$120（半紙・墨液など込み）道具は貸し出しいたします。
-          </p>
-          <p>
-            (参考課題)<br />
-            小1 「かさ」<br />
-            小2 「げんき」<br />
-            小3 「大地」<br />
-            小4 「草原」<br />
-            小5 「実現」<br />
-            小6 「旅に出る」<br />
-            ※経験者の場合は課題の難易度を上げることもできます。
-          </p>
-        </li>
-        <li class="leading-8">
-          <p class="mb-8 text-2xl">
-            （締め切りました）大人クラス・学生クラス生徒さん募集
-          </p>
-          <time class="block mb-8 text-lg" datetime="2021-01-28">
-            2021年1月28日
-          </time>
-          <p>
-            募集再開の情報はこちらのニュース欄またはインスタグラム（<a
-              href="https://www.instagram.com/sg_shodo_mh/"
-              class="underline"
-              target="_blank"
-              rel="noopener"
-              >@sg_shodo_mh</a
-            >）にて公開いたします。
-          </p>
-        </li>
-      </ul>
+      <News
+        v-for="(item, index) in news"
+        :key="item.id"
+        :title="item.title"
+        :publish-date="item.publish_date"
+        :body="item.body"
+        :class="{ 'mb-24': index !== news.length - 1 }"
+      ></News>
     </PageSection>
 
     <div id="course" class="mb-24"></div>
@@ -246,9 +206,12 @@
 </template>
 
 <script>
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+import { BLOCKS } from '@contentful/rich-text-types'
 import ButtonLink from '@/components/ButtonLink'
 import ContactForm from '@/components/ContactForm'
 import Course from '@/components/Course'
+import News from '@/components/News'
 import PageSection from '@/components/PageSection'
 import SocialHead from '@/components/SocialHead'
 
@@ -257,8 +220,33 @@ export default {
     ButtonLink,
     ContactForm,
     Course,
+    News,
     PageSection,
     SocialHead,
+  },
+  async asyncData({ $contentful }) {
+    const renderOptions = {
+      renderNode: {
+        [BLOCKS.PARAGRAPH]: (node, next) =>
+          `<p class="mb-4">${next(node.content).replace(/\n/g, `</br>`)}</p>`,
+      },
+    }
+
+    const { items } = await $contentful.getEntries({
+      content_type: 'news',
+      order: '-fields.publish_date',
+    })
+
+    const news = items.map((item) => {
+      return {
+        id: item.sys.id,
+        title: item.fields.title,
+        publish_date: item.fields.publish_date,
+        body: documentToHtmlString(item.fields.body, renderOptions),
+      }
+    })
+
+    return { news }
   },
   computed: {
     mainImageUrl() {
